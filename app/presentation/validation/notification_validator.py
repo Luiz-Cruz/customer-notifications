174581dcd8011a7dc31f007b7b397ccf7472530d
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-19 17:55:34 -0300

    feat: add exception treatment

--- app/presentation/validation/notification_validator.py
+++ app/presentation/validation/notification_validator.py
@@ -1,20 +1,21 @@
 from functools import wraps
 from flask import request, jsonify
 from marshmallow import ValidationError
+from loguru import logger
+from app.domain.errors.bad_request import BadRequest
 
 from app.presentation.models.notifications_body import NotificationBody
-
 
 def validate_notifications_request(schema):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            json_data = request.get_json()
             try:
-                json_data = request.get_json()
-                data = schema.load(json_data)
+                data = schema().load(json_data)
                 validated_data = NotificationBody(**data)
                 return func(validated_data, *args, **kwargs)
             except ValidationError as err:
-                return jsonify({'error': err.messages}), 400
+                raise BadRequest(f"Invalid request body, missing: {err.messages}")            
         return wrapper
     return decorator
