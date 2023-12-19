8a94162bc672a5ce2b3c6c10fce2461eeddb9b9e
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-19 17:54:49 -0300

    feat: request schema validator

--- app/presentation/schemas/notifications.py
+++ app/presentation/schemas/notifications.py
@@ -1,24 +1,4 @@
-from datetime import datetime
-
 from marshmallow import Schema, fields
-
-from app.domain.errors.bad_request import BadRequest
-
-
-def validate_notification_type(notification_type):
-    if notification_type not in ['email', 'sms', 'push']:
-        raise BadRequest(f"Invalid notification type: {notification_type}")
-    return notification_type
-
-
-def validate_schedule_date(schedule_date):
-    if schedule_date:
-        try:
-            datetime.strptime(schedule_date, '%Y-%m-%d %H:%M:%S')
-        except ValueError:
-            raise BadRequest(f"Invalid schedule date: {schedule_date}, expected format: YYYY-MM-DD HH:MM:SS")
-    return schedule_date
-
 
 class NotificationSchema(Schema):
     """
@@ -31,5 +11,5 @@ class NotificationSchema(Schema):
     """
     user_id = fields.Str(required=True)
     message = fields.Str(required=True)
-    notification_type = fields.Str(required=True, validate=validate_notification_type)
-    schedule_date = fields.Str(required=False, validate=validate_schedule_date)
+    notification_type = fields.Str(required=True)
+    schedule_date = fields.Str(required=False)
