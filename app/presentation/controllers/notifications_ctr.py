9eb1f7ed2e983545ca014a23aa53b016733d75f8
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-19 22:51:03 -0300

    feat: add notifications controller with create_notification endpoint

---
+++ app/presentation/controllers/notifications_ctr.py
@@ -0,0 +1,26 @@
+from flask import Response, Blueprint, jsonify
+from loguru import logger
+
+from app.adapters.celery_broker_adapter import CeleryMessageBroker
+from app.adapters.mongodb_adapter import MongoDBAdapter
+from app.adapters.notification_processor_adapter import NotificationProcessor
+
+from app.domain.http.status_code import HttpStatusCode
+from app.presentation.models.notifications_body import NotificationBody
+from app.presentation.schemas.notifications import NotificationSchema
+from app.presentation.validation.notification_validator import validate_notifications_request
+
+
+mongodb = MongoDBAdapter()
+celery = CeleryMessageBroker()
+notification_processor = NotificationProcessor(mongodb, celery)
+notifications_router = Blueprint("notifications_router", __name__)
+
+
+@notifications_router.route("/notifications", methods=["POST"])
+@validate_notifications_request(NotificationSchema)
+def create_notification(body: NotificationBody) -> Response:
+    """Creates a new customer notification"""
+    logger.info(f"Received notification request: {body}")
+    notification_processor.execute(body)
+    return jsonify({"message": "Notification received successfully"}), HttpStatusCode.CREATED
