84abbb522be0e1d3141214a4b292efcd7d2c4e20
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-20 09:55:06 -0300

    ref: chand raise method

--- app/domain/errors/error_handler.py
+++ app/domain/errors/error_handler.py
@@ -1,11 +1,17 @@
 from flask import Response
-
-from app.adapters.http_response_adapter import respond_api_error
-
+from app.adapters.http.http_response_adapter import respond_api_error
 
 def handle_exception(ex: Exception) -> Response:
+    """
+    Handle exceptions by generating an API error response.
+
+    Args:
+        ex (Exception): The exception to be handled.
+
+    Returns:
+        Response: The API error response generated from the exception.
+    """
     return respond_api_error(
         ex.get_message(),
         ex.get_response_code()
     )
-    
--- app/ports/message_broker.py
+++ app/ports/message_broker.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class MessageBroker(ABC):
     @abstractmethod
-    def send_to_queue(self, message):
-        pass
+    def execute(self, message):
+        raise NotImplementedError
