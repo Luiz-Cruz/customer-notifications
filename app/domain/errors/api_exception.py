af3113468613d5368d237eece9b8d189ccb7270d
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-19 22:50:18 -0300

    ref: refactor ApiException constructor parameters order

--- app/domain/errors/api_exception.py
+++ app/domain/errors/api_exception.py
@@ -3,9 +3,9 @@ class ApiException(Exception):
     API exceptions class to handle api exceptions in the application.
     """
 
-    def __init__(self, response_code: int, message: str):
-        self.response_code = response_code
+    def __init__(self, message: str, response_code: int,):
         self.message = message
+        self.response_code = response_code
 
     def get_response_code(self):
         """
