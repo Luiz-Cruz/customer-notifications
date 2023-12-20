703e3dcd2a1b3cbdb8ecae4c513584d5a10aeeea
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-20 09:55:38 -0300

    style: add docstring to validator

--- app/presentation/validation/notification_validator.py
+++ app/presentation/validation/notification_validator.py
@@ -3,13 +3,34 @@ from flask import request, jsonify
 from marshmallow import ValidationError
 from loguru import logger
 from app.domain.errors.bad_request import BadRequest
-
 from app.presentation.models.notifications_body import NotificationBody
 
 def validate_notifications_request(schema):
+    """
+    Validate notifications request decorator.
+
+    Args:
+        schema: The schema used for validation.
+
+    Returns:
+        function: Decorator function.
+    """
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            """
+            Wrapper function to validate notifications request.
+
+            Args:
+                *args: Variable length argument list.
+                **kwargs: Arbitrary keyword arguments.
+
+            Raises:
+                BadRequest: If the request body is invalid.
+
+            Returns:
+                Response: Response from the decorated function.
+            """
             json_data = request.get_json()
             try:
                 data = schema().load(json_data)
