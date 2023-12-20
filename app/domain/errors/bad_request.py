eda2ead61f3938e105c21bace75d3d1b41129f84
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-20 11:08:22 -0300

    ref: refactor BadRequest constructor signature

--- app/domain/errors/bad_request.py
+++ app/domain/errors/bad_request.py
@@ -4,4 +4,4 @@ from app.domain.errors.api_exception import ApiException
 
 class BadRequest(ApiException):
     def __init__(self, error_msg: str):
-        super().__init__(HttpStatusCode.BAD_REQUEST, error_msg)
+        super().__init__(error_msg, HttpStatusCode.BAD_REQUEST)
