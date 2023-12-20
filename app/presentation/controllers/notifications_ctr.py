a54010be3b046b8b4251c80b3195e25100f973e0
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-12-20 09:56:07 -0300

    ref: change repository name

--- app/presentation/controllers/notifications_ctr.py
+++ app/presentation/controllers/notifications_ctr.py
@@ -1,9 +1,11 @@
 from flask import Response, Blueprint, jsonify
 from loguru import logger
 
-from app.adapters.celery_broker_adapter import CeleryMessageBroker
-from app.adapters.mongodb_adapter import MongoDBAdapter
-from app.adapters.notification_processor_adapter import NotificationProcessor
+from app.adapters.queue.celery_broker_adapter import CeleryMessageBroker
+from app.adapters.repository.mongodb_notification_adapter import MongoDBNotificationAdapter
+from app.adapters.repository.user_mongodb_adapter import UserRepositoryMongoDBAdapter
+from app.adapters.processor.notification_processor_adapter import NotificationProcessor
+from app.domain.entities.notification import Notification
 from app.presentation.validation.notification_validator import validate_notifications_request
 from app.domain.http.status_code import HttpStatusCode
 from app.presentation.models.notifications_body import NotificationBody
@@ -11,7 +13,7 @@ from app.presentation.schemas.notifications import NotificationSchema
 from app.presentation.validation.notification_validator import validate_notifications_request
 
 
-mongodb = MongoDBAdapter()
+mongodb = UserRepositoryMongoDBAdapter()
 celery = CeleryMessageBroker()
 notification_processor = NotificationProcessor(mongodb, celery)
 notifications_router = Blueprint("notifications_router", __name__)
@@ -23,4 +25,7 @@ def create_notification(body: NotificationBody) -> Response:
     """Creates a new customer notification"""
     logger.info(f"Received notification request: {body}")
     notification_processor.execute(body)
-    return jsonify({"message": "Notification received successfully"}), HttpStatusCode.CREATED
+    return jsonify({
+        "message": "Notification received successfully", 
+        "status": HttpStatusCode.CREATED
+    }), HttpStatusCode.CREATED
